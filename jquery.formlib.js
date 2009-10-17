4d7ec5a7bb0984f22f68cfed6bbf3dea2e486c1b
Christian Schilling <christian@initcrash.net>
2009-10-17 16:47:52 +0200

    serialization works

--- jquery.formlib.js
+++ jquery.formlib.js
@@ -2,35 +2,75 @@
 
 $.csjforms= {
     widgets:{
-        text: {template:'<label><%= name %></label><input type="text" name="<%= name %>">'},
-        textarea: {template:'<label><%= name %></label><textarea rows=20 cols=80 name="<%= name %>"</textarea>'},
+        text: {
+            template:'<label><%= label %></label><input type="text" name="<%= name %>">',
+            tojson:function(val){return val;},
+        },
+        textarea: {
+            template:'<label><%= label %></label><textarea rows=20 cols=80 name="<%= name %>"></textarea>',
+            tojson:function(val){return val;},
+        },
     },
     templates:{
-        fieldset:'<fieldset id="<%= name %>"><legend><%= name %></legend></fieldset>',
-        inline:'<div id="<%= name %>"><input type="submit" name="add_<%= name %>" value="+"></div>',
-    },
+        fieldset:'<fieldset><legend><%= label %></legend><input type="submit" name="del_<%= name %>" value="-"></fieldset>',
+        inline:'<div class="formset" title="<%= name %>"><h1><%= label %></h1><input type="submit" name="add_<%= name %>" value="+"></div>',
+    }, }; widgets = $.csjforms.widgets; templates = $.csjforms.templates;
+
+function ucfirst(str) {
+    var firstLetter = str.substr(0, 1);
+    return firstLetter.toUpperCase() + str.substr(1);
+}
+function to_verbose(name,opts) {
+    if(opts.verbose) return opts.verbose;
+    else return ucfirst(name);
+}
+function to_plural(name,opts) {
+    if(opts.plural) return opts.plural;
+    else return to_verbose(name,opts)+'s';
+}
+
+tojson = function(def,obj) {
+    var jsonobj= new Object();
+    if(obj.is("fieldset")) {
+        obj.children().each(function(){
+            var id = $(this).attr('name') || $(this).attr('title');
+            if(id && def[id]) {
+                val = tojson(def[id],$(this));
+                jsonobj[id] = val;
+            }
+        });
+        return jsonobj;
+    } else if(obj.parent().is("fieldset")){
+        if(!def.inline){
+            return obj.val();
+        }else{
+            var jsonarray = [];
+            obj.children("fieldset").each(function(){
+                val = tojson(def.inline,$(this));
+                jsonarray.push(val);
+            });
+            return jsonarray;
+        }
+    };
 };
-widgets = $.csjforms.widgets;
-templates = $.csjforms.templates;
 
 $.fn.csjfieldset = function(def) {
 
     make_field = function(fieldname,opts) {
-        return template(opts.widget.template,{name:fieldname});
+        return template(opts.widget.template,{name:fieldname,label:to_verbose(fieldname,opts)});
     };
 
     obj = this;
     $.each(def,function(fieldname,opts){
         if(opts.inline){
-            obj.append(template(templates.inline,{name:fieldname}))
-               .children("div#"+fieldname)
+            obj.append(template(templates.inline,{name:fieldname,label:to_plural(fieldname,opts)}))
+               .children("div[title="+fieldname+']')
                .children("input[value=+]")
                .click(function(){
-                   subfieldname = fieldname+'__'+$(this).parent().children("fieldset").size();
-                   $(this).parent()
-                          .append(template(templates.fieldset,{name:subfieldname}))
-                          .children("fieldset#"+subfieldname)
-                          .csjfieldset(opts.inline);
+                    $(this).before(template(templates.fieldset,{name:fieldname,label:to_verbose(fieldname,opts)}))
+                           .prev()
+                           .csjfieldset(opts.inline)
+                           .children("input[value]=-").click(function(){$(this).parent().remove();});
                 });
         } else {
             obj.append(make_field(fieldname,opts));
@@ -40,10 +80,17 @@ $.fn.csjfieldset = function(def) {
 };
 
 $.fn.csjform = function(def){
-    return this.submit(function(){return false;})
-               .prepend(template(templates.fieldset,{name:def.name}))
-               .children("fieldset#"+def.name)
-               .csjfieldset(def.form);
+    this.submit(function(){return false;})
+        .prepend(template(templates.fieldset,{name:def.name,label:to_verbose(def.name,def)}))
+        .children("fieldset")
+        .csjfieldset(def.fields);
+
+    var obj = this.children("fieldset").eq(0);
+
+    this.find("input#save").click(function(){
+        alert(tojson(def.fields,obj).toSource());
+    });
+
 };
 
 })(jQuery);
