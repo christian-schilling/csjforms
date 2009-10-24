75a54c1354ec86ea23d484b67a77b788c9636c65
Christian Schilling <christian@initcrash.net>
2009-10-24 03:25:06 +0200

    finally a completly recursion based implementation

--- jquery.formlib.js
+++ jquery.formlib.js
@@ -1,4 +1,4 @@
-(function($){
+(function(){
 
 var extend = function(dest,src) {
     for(var i in src) {
@@ -7,19 +7,39 @@ var extend = function(dest,src) {
     return dest;
 };
 
-$.csjforms = {
+var ucfirst = function(str) {
+    var firstLetter = str.substr(0, 1);
+    return firstLetter.toUpperCase() + str.substr(1);
+}
+var to_verbose = function(opts) {
+    if(opts.verbose) return opts.verbose;
+    else return ucfirst(opts.name);
+}
+var to_plural = function(opts) {
+    if(opts.plural) return opts.plural;
+    else return to_verbose(opts)+'s';
+}
+
+
+csjforms = {
     templates:{
         fieldset:'<fieldset><legend><%= label %></legend></fieldset>',
-        inlinefieldset:'<fieldset><legend><%= label %></legend><input type="submit" name="del_<%= name %>" value="-" class="delbutton"></fieldset>',
     },
     widgets:{
         text: function(options) {
             return extend({
                 template : '<label><%= label %></label><input type="text" name="<%= name %>">',
+                create : function(parentdef,jqs) {
+                    jqs.append(template(this.template,{name:parentdef.name,label:to_verbose(parentdef)}));
+                    var jqs = jqs.children('[name='+this.name+']');
+                    return jqs;
+                },
+                tojson : function(jqs) { return jqs.val(); },
+                fromjson : function(jqs,jsonobj) { jqs.val(jsonobj);}
             },options);
         },
         textarea: function(options) {
-            return extend(extend($.csjforms.widgets.text(),{
+            return extend(extend(csjforms.widgets.text(),{
                 template : '<label><%= label %></label><textarea rows=20 cols=80 name="<%= name %>"></textarea>',
             }),options);
         },
@@ -27,126 +47,92 @@ $.csjforms = {
     fields:{
         text: function(options) {
             return extend({
-                widget : $.csjforms.widgets.text(),
-                tojson : function(val){return val;},
+                widget : csjforms.widgets.text(),
+                tojson : function(jqs){
+                    return this.widget.tojson(jqs.children('[name='+this.name+']'));
+                },
+                fromjson : function(jqs,jsonobj){
+                    this.widget.fromjson(jqs.children('[name='+this.name+']'),jsonobj);
+                },
+                create : function(parentdef,jqs) {
+                    jqs.append('<div class="csjformfield" title="'+this.name+'"></div>');
+                    var jqs = jqs.children('.csjformfield:last');
+                    this.widget.create(this,jqs);
+                    return jqs;
+                },
             },options);
         },
     },
-    docs:{
-        main: function(options) {
-            return options
-        },
-        inline: function(options) {
-            return extend({
-                template :'<div class="csjformset" title="<%= name %>">'
-                         +'    <h1><%= label %></h1>'
-                         +'    <input type="submit" name="add_<%= name %>" value="+" class="addbutton">'
-                         +'</div>',
-                doc : {},
-            },options);
-        },
+    fieldset: function(options) {
+        return extend({
+            template:'<fieldset><legend><%= label %></legend></fieldset>',
+            delbutton:'<input type="submit" name="del_<%= name %>" value="-" class="delbutton">',
+            fields : [],
+            create : function(parentdef,jqs) {
+                jqs.append(template(this.template,{name:this.name,label:to_verbose(this)}));
+                var jqs = jqs.children('fieldset:last');
+                if(parentdef) {
+                    jqs.append(template(this.delbutton,{name:this.name}));
+                    jqs.children('input.delbutton').click(function(){$(this).parent().remove();return false;});
+                }
+                for(var i in this.fields) {
+                    this.fields[i].create(this,jqs);
+                }
+                return jqs;
+            },
+            tojson : function(jqs) {
+                var jsonobj = {};
+                var field;
+                for(var i in this.fields) {
+                    field = this.fields[i];
+                    jsonobj[field.name] = field.tojson(jqs.children('[title='+field.name+']'));
+                }
+                return jsonobj;
+            },
+            fromjson : function(jqs,jsonobj){
+                var field;
+                for(var i in this.fields) {
+                    field = this.fields[i];
+                    field.fromjson(jqs.children('[title='+field.name+']'),jsonobj[field.name]);
+                }
+            },
+        },options);
+    },
+    inline: function(options) {
+        return extend({
+            template :'<div class="csjformset" title="<%= name %>">'
+                     +'    <h1><%= label %></h1>'
+                     +'    <input type="submit" name="add_<%= name %>" value="+" class="addbutton">'
+                     +'</div>',
+            create : function(parentdef,jqs) {
+                jqs.append(template(this.template,{name:this.name,label:to_verbose(this)}));
+                var jqs = jqs.children('.csjformset[title='+this.name+']');
+                var that = this;
+                jqs.children('input.addbutton[name=add_'+this.name+']').click(function(){
+                    that.append_fieldset($(this).parent());
+                    return false;
+                });
+                return jqs;
+            },
+            append_fieldset : function(jqs) {
+                return this.fieldset.create(this,jqs);
+            },
+            tojson : function(jqs) {
+                var jsonobj = [];
+                var that = this;
+                jqs.children('fieldset').each(function(){
+                    jsonobj.push(that.fieldset.tojson($(this)));
+                });
+                return jsonobj;
+            },
+            fromjson : function(jqs,jsonobj){
+                for(var i in jsonobj){
+                    this.fieldset.fromjson(this.append_fieldset(jqs),jsonobj[i]);
+                }
+            },
+        },options);
     },
 };
 
-function ucfirst(str) {
-    var firstLetter = str.substr(0, 1);
-    return firstLetter.toUpperCase() + str.substr(1);
-}
-function to_verbose(name,opts) {
-    if(opts.verbose) return opts.verbose;
-    else return ucfirst(name);
-}
-function to_plural(name,opts) {
-    if(opts.plural) return opts.plural;
-    else return to_verbose(name,opts)+'s';
-}
-
-append_fieldset = function(formset,fieldname,opts) {
-    formset.children('.addbutton').before(
-        template($.csjforms.templates.inlinefieldset, {
-            name:fieldname,label:to_verbose(fieldname,opts)
-        })
-    );
-
-    formset.children('fieldset:last')
-           .csjfieldset(opts.doc)
-           .children("input.delbutton").click(function(){$(this).parent().remove();});
-};
-
-tojson = function(def,obj) {
-    if(obj.is("fieldset")) {
-        var jsonobj = new Object();
-        obj.children('.csjformset,.csjformfield').each(function(){
-            var id = $(this).attr('title');
-            jsonobj[id] = tojson(def[id],$(this));
-        });
-    }else if(obj.is('.csjformset')){
-        var jsonobj = [];
-        obj.children("fieldset").each(function(){
-            val = tojson(def.doc,$(this));
-            jsonobj.push(val);
-        });
-    }else if(obj.is(".csjformfield")){
-        jsonobj = obj.find('input,textarea,select').val();
-    }
-    return jsonobj;
-};
-
-fromjson = function(def,obj,jsonobj) {
-    if(obj.is("fieldset")) {
-        obj.children('.csjformset,.csjformfield').each(function(){
-            var id = $(this).attr('title');
-            fromjson(def[id],$(this),jsonobj[id],id);
-        });
-    }else if(obj.is('.csjformset')){
-        for(var i in jsonobj){
-            append_fieldset(obj,obj.attr('title'),def);
-            fromjson(def.doc,obj.children('fieldset:last'),jsonobj[i]);
-        }
-    }else if(obj.is(".csjformfield")){
-        obj.find('input,textarea,select').val(jsonobj);
-    }
-};
-
-$.fn.csjfieldset = function(def) {
-
-    make_field = function(fieldname,opts) {
-        return '<div class="csjformfield" title="'+fieldname+'">'
-              +template(opts.widget.template,{
-                    name:fieldname,
-                    label:to_verbose(fieldname,opts)
-               })
-              +'</div>';
-    };
-
-    var obj = this;
-    $.each(def,function(fieldname,opts){
-        if(opts.doc){
-            obj.append(template(opts.template,{name:fieldname,label:to_plural(fieldname,opts)}))
-               .children("div[title="+fieldname+']')
-               .children("input.addbutton")
-               .click(function(){ append_fieldset($(this).parent(),fieldname,opts); });
-        } else if (opts.widget) {
-            obj.append(make_field(fieldname,opts));
-        }
-    });
-    return this;
-};
-
-$.fn.csjform = function(def,callback,data){
-    this.submit(function(){return false;})
-        .prepend(template($.csjforms.templates.fieldset,{name:def.name,label:to_verbose(def.name,def)}))
-        .children("fieldset")
-        .csjfieldset(def.doc);
-
-    var obj = this.children("fieldset").eq(0);
-
-    this.find("input#save").click(function(){
-        callback(tojson(def.doc,obj));
-    });
-
-    if(data) fromjson(def.doc,obj,data);
-};
-
-})(jQuery);
+})();
 
